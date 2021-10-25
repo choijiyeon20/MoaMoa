237096e87b42c4440b759b26cbb206e5543ba5d5
Cat Hanbit <dreamlmy1203@naver.com>
2021-10-25 16:47:34 +0900

    이민영 -메인 홈 사진 6개 넣기 -메인페이지 이동(구현 안됨, 로그캣 오류 없음)

---
+++ app/src/main/java/com/cookandroid/moamoa/mainhomemypage.java
@@ -0,0 +1,46 @@
+package com.cookandroid.moamoa;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class mainhomemypage extends Fragment {
+    private View view;
+
+    //네비바 외의 프레그먼트와 연결할 때 꼭 필요한 newlnstnce() 메소드
+    public static mainhomemypage newlnstnce(){
+        return new mainhomemypage();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.main_home, container, false);
+
+        //FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+
+        ImageButton button = (ImageButton) view.findViewById(R.id.mine);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).replaceFragment(Main_mypage.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+
+
+            }
+        });
+        return view;
+    }
+
+}
