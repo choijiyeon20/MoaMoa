855f2078e054bd7e2611b7bcf0070d5a16e5f3fe
aralion <aadsf1789@gmail.com>
2021-10-26 17:43:57 +0900

    연결

---
+++ app/src/main/java/com/cookandroid/moamoa/MoAMoA_Login.java
@@ -0,0 +1,28 @@
+package com.cookandroid.moamoa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoAMoA_Login extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.moamoa_login);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        Button resistButton = (Button) findViewById(R.id.go_resist);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
