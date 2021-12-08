a83f10476f42e4c443d4317dd9e7922836be4fc2
Cat Hanbit <dreamlmy1203@naver.com>
2021-12-09 02:56:51 +0900

    이민영 메인 홈: 버튼 19개 페이지 연결 프로필->마이페이지 연결 마이페이지: 6개 버튼 페이지 각각 연결, 연결 페이지 레이아웃 초안 제작

--- app/src/main/java/com/cookandroid/moamoa/Main_home.java
+++ app/src/main/java/com/cookandroid/moamoa/Main_home.java
@@ -3,11 +3,13 @@ package com.cookandroid.moamoa;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -33,7 +35,7 @@ public class Main_home extends Fragment {
         //FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
 
 
-        ImageView button = (ImageView) view.findViewById(R.id.btnBoard);
+        Button button = (Button) view.findViewById(R.id.btnBoard);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,7 +48,219 @@ public class Main_home extends Fragment {
 
             }
         });
+        Button button1 = (Button) view.findViewById(R.id.btnBoard2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+
+
+            }
+        });
+        Button button2 = (Button) view.findViewById(R.id.btnBoard3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        Button button3 = (Button) view.findViewById(R.id.btnBoard4);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        Button button4 = (Button) view.findViewById(R.id.btnBoard5);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button5 = (ImageButton) view.findViewById(R.id.homing);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        Button button6 = (Button) view.findViewById(R.id.hobutton);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button7 = (ImageButton) view.findViewById(R.id.rank);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        Button button8 = (Button) view.findViewById(R.id.ranking);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        Button button9 = (Button) view.findViewById(R.id.recent);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button10 = (ImageButton) view.findViewById(R.id.reim);
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        Button button11 = (Button) view.findViewById(R.id.tr);
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button12 = (ImageButton) view.findViewById(R.id.good);
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button13 = (ImageButton) view.findViewById(R.id.first);
+        button13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button14 = (ImageButton) view.findViewById(R.id.second);
+        button14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button15 = (ImageButton) view.findViewById(R.id.third);
+        button15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button16 = (ImageButton) view.findViewById(R.id.mmh);
+        button16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button17 = (ImageButton) view.findViewById(R.id.last);
+        button17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+        ImageButton button18 = (ImageButton) view.findViewById(R.id.you);
+        button18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(Board.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+
+        ImageButton button19 = (ImageButton) view.findViewById(R.id.mine);
+        button19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(mine.newlnstnce());
+                //activity.onFragmentChange(1);
+                /* 액티비티일 때 띄우기
+                Intent intent = new Intent(getActivity(), Board.class);
+                startActivity(intent);*/
+            }
+        });
+
         return view;
     }
-
 }
